59bfdda49c6e047c2f97e380eaa15e94a004a98c
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 21:29:42 +0300

    feat: made decorator for bot's exec functions

---
+++ IMEI-Telegram-Bot/imei_bot/run_bot.py
@@ -0,0 +1,24 @@
+import structlog
+
+from settings import Settings
+
+logger = structlog.get_logger(__name__)
+
+
+def bot_logger(function):
+    def wrapper(*args, **kwargs):
+        logger.info(
+            f'''
+            Start running {function.__name__} 
+            With settings and arguments
+            ''',
+            settings=f'{Settings}',
+            args=args,
+            kwargs=kwargs
+        )
+        try:
+            return function(*args, **kwargs)
+        except Exception as exc:
+            logger.error('Exception occurred', exc_info=exc)
+
+    return wrapper
