5497c194dea94364ccf5fded57b048ebd18f61b8
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 22:23:06 +0300

    feat: import that decorator and hung on beginning handler

--- IMEI-Telegram-Bot/handlers/greeting.py
+++ IMEI-Telegram-Bot/handlers/greeting.py
@@ -2,11 +2,13 @@ import structlog
 from aiogram import Router, types
 
 from settings import Settings
+from helpers.decorators import bot_logger
 
 logger = structlog.get_logger(__name__)
 router = Router()
 
 
+@bot_logger
 @router.message()
 async def start(message: types.Message):
     await message.answer('hello')
