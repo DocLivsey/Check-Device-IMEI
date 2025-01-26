e8fa00668364a0d73c30b2230e8437d174660ac8
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 21:22:47 +0300

    feat: init bot object

--- IMEI-Telegram-Bot/imei_bot/IMEIbot.py
+++ IMEI-Telegram-Bot/imei_bot/IMEIbot.py
@@ -1,3 +1,9 @@
 import structlog
+from aiogram import Bot
+
+from settings import Settings
 
 logger = structlog.get_logger(__name__)
+
+bot_token = Settings.TELEGRAM_BOT_TOKEN_KEY.value
+imei_bot = Bot(token=bot_token)
