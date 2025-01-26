6cf46b85591949cef0305937007df61e07f2e9e7
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 21:35:52 +0300

    feat: created main runnable method

--- IMEI-Telegram-Bot/imei_bot/IMEIbot.py
+++ IMEI-Telegram-Bot/imei_bot/IMEIbot.py
@@ -1,9 +1,27 @@
 import structlog
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from settings import Settings
 
 logger = structlog.get_logger(__name__)
 
 bot_token = Settings.TELEGRAM_BOT_TOKEN_KEY.value
-imei_bot = Bot(token=bot_token)
+check_imei_bot = Bot(token=bot_token)
+storage = MemoryStorage()
+dispatcher = Dispatcher(storage=storage)
+
+
+async def run():
+    """
+    Main loop functions for polling telegram bot
+    """
+    await check_imei_bot.delete_webhook(
+        drop_pending_updates=True
+    )
+
+    await dispatcher.start_polling(
+        check_imei_bot,
+    )
+
+
