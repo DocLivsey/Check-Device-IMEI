668a2cb05ebcc82bebd70faa33980d478fd248eb
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-26 22:18:33 +0300

    feat: added start dialog handling method

--- IMEI-Telegram-Bot/handlers/greeting.py
+++ IMEI-Telegram-Bot/handlers/greeting.py
@@ -1,7 +1,12 @@
 import structlog
-from aiogram import Router
+from aiogram import Router, types
 
 from settings import Settings
 
 logger = structlog.get_logger(__name__)
 router = Router()
+
+
+@router.message()
+async def start(message: types.Message):
+    await message.answer('hello')
