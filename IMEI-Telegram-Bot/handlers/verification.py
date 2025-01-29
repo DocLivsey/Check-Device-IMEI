6794ab0f7a1233dfba547eac29f25ef6477ae0d2
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-29 15:18:04 +0300

    feat: some method in verification for check imei

---
+++ IMEI-Telegram-Bot/handlers/verification.py
@@ -0,0 +1,15 @@
+import structlog
+from aiogram import Router, types
+from aiogram.filters import Command
+
+from settings import Settings
+from helpers.decorators import bot_logger
+
+logger = structlog.get_logger(__name__)
+router = Router()
+
+
+@bot_logger
+@router.message(Command('check-imei'))
+async def start_handler(message: types.Message):
+    await message.answer('hello')
