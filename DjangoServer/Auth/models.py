717729e78fae113daa924de85dc9d759641c1c23
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-30 21:33:34 +0300

    feat: added a table of users with Telegram IDs

--- DjangoServer/Auth/models.py
+++ DjangoServer/Auth/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class TelegramUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telegram_id = models.BigIntegerField(unique=True)
