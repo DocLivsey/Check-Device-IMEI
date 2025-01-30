b628fda03b3a6e5513a6979ac60d440e2ff74ee6
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-31 01:56:35 +0300

    feat: created new view class for greeting with user if he exists in system

--- DjangoServer/CheckIMEI/views.py
+++ DjangoServer/CheckIMEI/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.db import OperationalError
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        try:
+            user = request.user
+            return Response({'message': f'Hello, {user.username}!'})
+        except OperationalError:
+            return Response({'message': 'Who are you?'})
