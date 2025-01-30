3a68e488c8c63d37811112bce2e19d2e5e942667
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-30 21:25:56 +0300

    feat: created new view class as test token auth for api responses and added it into urls

--- DjangoServer/DjangoServer/urls.py
+++ DjangoServer/DjangoServer/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from Auth.views import HelloView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('hello/', HelloView.as_view(), name='hello'),
 ]
