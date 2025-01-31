f8401c0f6ea1a6da141a98b0e0dce547e3df448f
DocLivsey <doclivsey@users.noreply.github.com>
2025-01-31 15:27:32 +0300

    feat: added handler method for /hello endpoint

--- API/v1/endpoints/check_imei/checkIMEI.py
+++ API/v1/endpoints/check_imei/checkIMEI.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from v1.schemas.checkIMEI import HelloScheme
+
 check_imei_router = APIRouter()
+
+
+@check_imei_router.get('/hello', response_model=HelloScheme)
+async def hello():
+    pass
