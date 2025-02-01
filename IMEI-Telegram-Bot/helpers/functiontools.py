f3f3623c9b924a227e2088823d463ab31194b769
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-01 17:15:49 +0300

    feat: started new method for checking user's token

--- IMEI-Telegram-Bot/helpers/functiontools.py
+++ IMEI-Telegram-Bot/helpers/functiontools.py
@@ -1,3 +1,13 @@
+import requests
 import structlog
 
+from settings import Settings
+
 logger = structlog.get_logger(__name__)
+
+
+def auth_required(users_tokens: dict, user_id: int):
+    if not users_tokens[user_id]:
+        response = requests.post(
+            url=f'{Settings.API_HOST}{Settings.API_BASE_PATH}{Settings.API_VERSION}{Settings.API_URL_TAKE_TOKEN}',
+        )
