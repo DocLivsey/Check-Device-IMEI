18e133a35c7c2e04ee897aeceb44e8ee2d36ec9e
DocLivsey <doclivsey@github.com>
2025-02-02 00:55:25 +0300

    feat: completed method to require auth

--- IMEI-Telegram-Bot/helpers/functiontools.py
+++ IMEI-Telegram-Bot/helpers/functiontools.py
@@ -11,3 +11,16 @@ def auth_required(users_tokens: dict, user_id: int):
         response = requests.post(
             url=f'{Settings.API_HOST}{Settings.API_BASE_PATH}{Settings.API_VERSION}{Settings.API_URL_TAKE_TOKEN}',
         )
+        if response.status_code != 200:
+            logger.error(
+                'Failed to get token', 
+                response=response
+            )
+            raise Exception('Not Authenticated')
+        
+        users_tokens[user_id] = response.json()['token']
+        
+        return
+    
+    return
+    
