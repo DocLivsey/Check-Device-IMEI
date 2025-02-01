ecfed4d95e033774ee2a906166ad849d059e415b
DocLivsey <doclivsey@github.com>
2025-02-02 01:05:01 +0300

    feat: splited functions into two for authenticate and check auth and added logs

--- IMEI-Telegram-Bot/helpers/functiontools.py
+++ IMEI-Telegram-Bot/helpers/functiontools.py
@@ -7,7 +7,36 @@ logger = structlog.get_logger(__name__)
 
 
 def auth_required(users_tokens: dict, user_id: int):
+    try:
+        authenticate(users_tokens, user_id)
+        
+        logger.info(
+            'User authenticated',
+            user_id=user_id
+        )
+        
+    except Exception as exception:
+        logger.error(
+            'Failed to authenticate user',
+            user_id=user_id,
+            exception=exception.with_traceback(),
+        )
+        
+        return
+
+
+def authenticate(users_tokens: dict, user_id: int):
+    logger.info(
+        'tring to authenticate user',
+        user=user_id
+    )
+    
     if not users_tokens[user_id]:
+        logger.info(
+            'user not authenticated, sending request to authenticate user',
+            user=user_id
+        )
+        
         response = requests.post(
             url=f'{Settings.API_HOST}{Settings.API_BASE_PATH}{Settings.API_VERSION}{Settings.API_URL_TAKE_TOKEN}',
         )
@@ -16,11 +45,17 @@ def auth_required(users_tokens: dict, user_id: int):
                 'Failed to get token', 
                 response=response
             )
+            
             raise Exception('Not Authenticated')
         
         users_tokens[user_id] = response.json()['token']
         
+        logger.info(
+            'User authenticated successfully',
+            user=user_id,
+            token=users_tokens[user_id]
+        )
+        
         return
     
     return
-    
