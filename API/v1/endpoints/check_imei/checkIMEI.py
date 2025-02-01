7b2138e1fbf90b6ad67e680da0ae99e11f485a9b
DocLivsey <doclivsey@github.com>
2025-02-01 13:44:25 +0300

    fix: missing backslash at endpoint path

--- API/v1/endpoints/check_imei/checkIMEI.py
+++ API/v1/endpoints/check_imei/checkIMEI.py
@@ -5,6 +5,6 @@ from v1.schemas.checkIMEI import HelloScheme
 check_imei_router = APIRouter()
 
 
-@check_imei_router.get('/hello', response_model=HelloScheme)
+@check_imei_router.get('/hello/', response_model=HelloScheme)
 async def hello():
     pass
