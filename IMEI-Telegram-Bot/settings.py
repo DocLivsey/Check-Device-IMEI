a0767c1a4e39ed6c655ccfb2136627ac037a38ad
DocLivsey <doclivsey@github.com>
2025-02-02 10:53:26 +0300

    feat: added new env var cause fastapi endpoints are longer

--- IMEI-Telegram-Bot/settings.py
+++ IMEI-Telegram-Bot/settings.py
@@ -12,6 +12,7 @@ class Settings(Enum):
     API_HOST = environ.get('API_URL', 'http://192.168.0.107:8080')
     API_BASE_PATH = environ.get('API_BASE_PATH', '/api')
     API_VERSION = environ.get('API_VERSION', '/v1')
+    API_AUTH_URL = environ.get('API_AUTH_URL', '/auth')
     API_URL_TAKE_TOKEN = environ.get('API_TAKE_TOKEN_URL', '/api-token-telegram-auth/')
 
     @staticmethod
