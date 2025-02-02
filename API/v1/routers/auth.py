bc1e8b118f03c355a5ea9505a59a9e09ab8394d5
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-02 18:22:35 +0300

    feat: importing /auth from settings

--- API/v1/routers/auth.py
+++ API/v1/routers/auth.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter
 
 from v1.endpoints.auth.auth import auth_router as auth_endpoints
 from v1.schemas.auth import TokenSchema
+from settings import api_auth_url
 
 router = APIRouter()
 
 router.include_router(
     auth_endpoints,
-    prefix="/auth",
+    prefix=str(api_auth_url),
     tags=["auth"],
     responses={
         500: {"description": "Internal Server Error"},
