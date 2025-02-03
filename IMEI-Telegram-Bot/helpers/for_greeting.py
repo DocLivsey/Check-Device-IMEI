34524461b8011a53c747bfb8289fdc8e004f565f
DocLivsey <doclivsey@github.com>
2025-02-03 10:09:01 +0300

    feat: created helper's functions .py script

---
+++ IMEI-Telegram-Bot/helpers/for_greeting.py
@@ -0,0 +1,11 @@
+import structlog
+
+logger = structlog.get_logger(__name__)
+
+
+def is_authenticated(users_tokens: dict, user_id: int) -> bool:
+    return user_id in users_tokens and users_tokens[user_id] is not None
+
+
+def start_handler_():
+    pass
