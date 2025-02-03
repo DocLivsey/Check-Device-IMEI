9632f637c28f8dc31a477ffd772ab2338b28c3ef
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-03 13:09:13 +0300

    feat: added a little bit handling in /hello/ endpoint

--- DjangoServer/CheckIMEI/views.py
+++ DjangoServer/CheckIMEI/views.py
@@ -10,6 +10,18 @@ class HelloView(APIView):
     def get(self, request):
         try:
             user = request.user
-            return Response({'message': f'Hello, {user.username}!'})
+            call_user = 'Anonymous'
+
+            if user.first_name:
+                call_user = user.first_name
+
+            elif user.last_name:
+                call_user = user.last_name
+
+            elif user.username:
+                call_user = user.username
+
+            return Response({'message': f'Hello, {call_user}!'})
+
         except OperationalError:
             return Response({'message': 'Who are you?'})
