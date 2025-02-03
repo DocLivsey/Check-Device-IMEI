11d8f275fcff41df8984c5113fb91f2b413cc169
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-03 12:17:04 +0300

    feat: init method for running calling in param handler logic if auth was passed

--- IMEI-Telegram-Bot/helpers/for_greeting.py
+++ IMEI-Telegram-Bot/helpers/for_greeting.py
@@ -7,5 +7,9 @@ def is_authenticated(users_tokens: dict, user_id: int) -> bool:
     return user_id in users_tokens and users_tokens[user_id] is not None
 
 
+def passed_auth_then_do():
+    pass
+
+
 def start_handler_():
     pass
