a8627dc7a92ad41805e348ba0e74ff3fca1bf65d
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-03 14:19:21 +0300

    feat: create mapping method

--- API/v1/schemas/checkIMEI.py
+++ API/v1/schemas/checkIMEI.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 
 class HelloScheme(BaseModel):
     message: str
+
+
+def to_message(message: str) -> HelloScheme:
+    if not message:
+        message = 'Undefined'
+
+    return HelloScheme(message=message)
