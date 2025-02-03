3f04627ffe1f2d65a8992fd527569739b6cddc10
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-04 00:50:04 +0300

    feat: added method to convert from dict

--- DjangoServer/CheckIMEI/responses.py
+++ DjangoServer/CheckIMEI/responses.py
@@ -1,3 +1,5 @@
+import uuid
+
 from CheckIMEI.models import Service, Property, IMEICheck
 
 
@@ -15,6 +17,13 @@ class ServiceResponse:
             title=service.title,
             price=service.price
         )
+
+    @staticmethod
+    def from_data(data: dict):
+        title = data['title'] if 'title' in data else 'Undefined'
+        price = data['price'] if 'price' in data else 'Undefined'
+
+        return ServiceResponse(title, price)
 
 
 class PropertiesResponse:
@@ -96,6 +105,46 @@ class PropertiesResponse:
             network=properties.network,
         )
 
+    @staticmethod
+    def from_data(data: dict):
+        device_name = data['device_name'] if 'device_name' in data else 'Undefined'
+        image = data['image'] if 'image' in data else 'Undefined'
+        imei = data['imei'] if 'imei' in data else 'Undefined'
+        est_purchase_date = data['est_purchase_date'] if 'est_purchase_date' in data else -1
+        sim_lock = data['sim_lock'] if 'sim_lock' in data else False
+        warranty_status = data['warranty_status'] if 'warranty_status' in data else 'Undefined'
+        repair_coverage = data['repair_coverage'] if 'repair_coverage' in data else 'Undefined'
+        technical_support = data['technical_support'] if 'technical_support' in data else 'Undefined'
+        model_desc = data['model_desc'] if 'model_desc' in data else 'Undefined'
+        demo_unit = data['demo_unit'] if 'demo_unit' in data else False
+        refurbished = data['refurbished'] if 'refurbished' in data else False
+        purchase_country = data['purchase_country'] if 'purchase_country' in data else 'Undefined'
+        apple_region = data['apple_region'] if 'apple_region' in data else 'Undefined'
+        fmi_on = data['fmi_on'] if 'fmi_on' in data else False
+        lost_mode = data['lost_mode'] if 'lost_mode' in data else 'Undefined'
+        usa_block_status = data['usa_block_status'] if 'usa_block_status' in data else 'Undefined'
+        network = data['network'] if 'network' in data else 'Undefined'
+
+        return PropertiesResponse(
+            device_name,
+            image,
+            imei,
+            est_purchase_date,
+            sim_lock,
+            warranty_status,
+            repair_coverage,
+            technical_support,
+            model_desc,
+            demo_unit,
+            refurbished,
+            purchase_country,
+            apple_region,
+            fmi_on,
+            lost_mode,
+            usa_block_status,
+            network,
+        )
+
 
 class IMEICheckResponse:
     id: str
@@ -142,4 +191,28 @@ class IMEICheckResponse:
             device_id=imei_check.device_id,
             processed_at=imei_check.processed_at,
             properties=PropertiesResponse.to_properties_response(imei_check.properties),
+        )
+
+    @staticmethod
+    def from_data(data: dict):
+        id = data['id'] if 'id' in data else str(uuid.uuid4())
+        type = data['type'] if 'type' in data else 'Undefined'
+        status = data['status'] if 'status' in data else 'Undefined'
+        order_id = data['order_id'] if 'order_id' in data else 'Undefined'
+        service = data['service'] if 'service' in data else {}
+        amount = data['amount'] if 'amount' in data else 'Undefined'
+        device_id = data['device_id'] if 'device_id' in data else 'Undefined'
+        processed_at = data['processed_at'] if 'processed_at' in data else -1
+        properties = data['properties'] if 'properties' in data else {}
+
+        return IMEICheckResponse(
+            id=id,
+            type=type,
+            status=status,
+            order_id=order_id,
+            service=ServiceResponse.to_service_response(service),
+            amount=amount,
+            device_id=device_id,
+            processed_at=processed_at,
+            properties=PropertiesResponse.to_properties_response(properties),
         )
