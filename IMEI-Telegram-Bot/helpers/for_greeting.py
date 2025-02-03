337b7a439d3fd11e127d86820f6b18b98175346e
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-03 12:25:11 +0300

    feat: created method for passing auth and doing handle logic and change method's name

--- IMEI-Telegram-Bot/helpers/for_greeting.py
+++ IMEI-Telegram-Bot/helpers/for_greeting.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 import structlog
 
 logger = structlog.get_logger(__name__)
@@ -7,9 +9,19 @@ def is_authenticated(users_tokens: dict, user_id: int) -> bool:
     return user_id in users_tokens and users_tokens[user_id] is not None
 
 
-def passed_auth_then_do():
-    pass
+def passed_auth_then_do(
+        users_tokens: dict,
+        user_id: int,
+        handler: Callable,
+        *args,
+        **kwargs
+):
+    if is_authenticated(users_tokens, user_id):
+        handler(users_tokens, *args, **kwargs)
+
+    else:
+        return
 
 
-def start_handler_():
+def start_handler_logic():
     pass
