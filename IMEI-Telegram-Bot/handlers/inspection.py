de6964d567306eb6622e06ecbf05f2847fbb81db
DocLivsey <doclivsey@users.noreply.github.com>
2025-02-04 16:53:33 +0300

    feat: added required auth

--- IMEI-Telegram-Bot/handlers/inspection.py
+++ IMEI-Telegram-Bot/handlers/inspection.py
@@ -3,6 +3,7 @@ import structlog
 from aiogram import Router, types
 from aiogram.filters import Command
 
+from helpers.functiontools import auth_required
 from settings import api_host, api_base_path, api_version
 from helpers.decorators import bot_logger
 
@@ -12,7 +13,12 @@ router = Router()
 
 @bot_logger
 @router.message(Command('check_imei'))
-async def check_imei_handler(message: types.Message):
+async def check_imei_handler(
+        message: types.Message,
+        users_tokens: dict
+):
+    await auth_required(users_tokens, message.from_user, message)
+
     response = requests.get(
         url=f'{api_host}{api_base_path}{api_version}/hello/',
         headers={
