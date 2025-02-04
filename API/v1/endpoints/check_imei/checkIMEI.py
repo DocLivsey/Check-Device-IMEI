39b73e29c27205a027604c2ef6edea87fcb611a1
DocLivsey <doclivsey@github.com>
2025-02-04 21:25:15 +0300

    feat: start handling sent request

--- API/v1/endpoints/check_imei/checkIMEI.py
+++ API/v1/endpoints/check_imei/checkIMEI.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request, HTTPException, status
 import structlog
 import requests
 
@@ -16,3 +16,39 @@ async def check_imei(request: Request):
         'Handle GET request to API for check device IMEI',
         request=request,
     )
+    
+    request_headers = request.headers
+    
+    if 'authorization' not in request_headers.keys():
+        logger.error(
+            'Authorization header missing',
+            request_headers=request.headers,
+            request_body=await request.body(),
+        )
+
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authorization header missing')
+
+    if not request_headers['Authorization'].startswith('Token '):
+        logger.error(
+            'Authorization header invalid',
+            request_headers=request.headers,
+            request_body=await request.body(),
+        )
+
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authorization header invalid')
+
+    if not request_headers['Authorization']:
+        logger.error(
+            'Authorization token missing',
+            request_headers=request.headers,
+            request_body=await request.body(),
+        )
+
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authorization token missing')
+    
+    url = f'{server_host}{api_base_path}{api_version}/check-imei/'
+    imei_check_url = 'https://api.imeicheck.net/v1/checks'
+    headers: dict = {
+        'Authorization': request_headers['authorization']
+    }
+    response: requests.Response
